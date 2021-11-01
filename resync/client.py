2a7fffd5b63b083529f53f1d9af2b3cb3046f156
Jan Funke <funkej@janelia.hhmi.org>
2021-11-01 18:01:07 -0400

    Add {read,write}_pdf to RemarkableClient, delete list_pdfs
    This change is in preparation for the fuse filesystem access.

--- resync/client.py
+++ resync/client.py
@@ -1,12 +1,14 @@
+from pathlib import Path
 import arrow
+import logging
 import os
 import paramiko
-import logging
+import tempfile
 
 from .constants import (
     PDF_BASE_METADATA,
     PDF_BASE_CONTENT)
-from .entries import Pdf, Folder
+from .entries import Pdf, Document
 from .filesystem import SshFileSystem
 from .index import RemarkableIndex
 from .lines import read_lines_document
@@ -30,28 +32,79 @@ class RemarkableClient:
         self.fs = SshFileSystem(self.ssh_client, self.document_root)
         self.index = RemarkableIndex(self.fs)
 
-    def list_pdfs(self, remote, recursive=False):
-        '''List all PDF documents under folder ``remote``.'''
+    def get_entry(self, path, typ=None):
 
-        if isinstance(remote, Folder):
-            folder = remote
-        else:
-            folder = self.index.get_entry_by_path(remote)
+        path = Path(path)
 
-        logger.debug("Listing PDFs in folder %s", folder)
+        entry = self.index.get_entry_by_path(path)
 
-        pdfs = [
-            entry
-            for entries in folder.files.values()
-            for entry in entries
-            if isinstance(entry, Pdf)
-        ]
+        if typ is not None:
+            if not isinstance(entry, typ):
+                raise RuntimeError(
+                    f"Expected to find {typ} at {path}, but found "
+                    f"{type(entry)}")
 
-        if recursive:
-            for subfolder in folder.folders.values():
-                pdfs += self.list_pdfs(subfolder, recursive=True)
+        return entry
 
-        return pdfs
+    def write_pdf(self, data, name, path):
+        '''Write a PDF with ``data`` as ``name`` in ``path``.'''
+
+        uid = self.index.create_new_uid()
+        parent_folder = self.index.get_entry_by_path(path)
+
+        meta = PDF_BASE_METADATA.copy()
+        meta['visibleName'] = name
+        meta['lastModified'] = str(arrow.utcnow().timestamp() * 1000)
+        meta['parent'] = parent_folder.uid
+
+        content = PDF_BASE_CONTENT.copy()
+
+        self.fs.write_file(data, uid + '.pdf')
+        self.fs.write_file(to_json(meta), uid + '.metadata')
+        self.fs.write_file(to_json(content), uid + '.content')
+        self.fs.write_file('', uid + '.pagedata')
+        self.fs.make_dir(uid)
+
+        entry = Pdf(uid, meta, content)
+        self.index.add_entry(entry)
+
+        return entry
+
+    def read_pdf(self, path):
+        '''Read PDF at ``path``, including annotations.'''
+
+        pdf = self.get_entry_by_path(path)
+        assert isinstance(pdf, Document)
+
+        with tempfile.TemporaryDirectory() as tmp_dir:
+
+            tmp_dir = Path(tmp_dir)
+            original_pdf = tmp_dir / pdf.uid + '.pdf'
+            annotations_pdf = tmp_dir / pdf.uid + '.annotations'
+            merged_pdf = tmp_dir / pdf.uid + '_merged.pdf'
+
+            # copy the original PDF to the temp dir
+            self.fs.get_file(pdf.uid + '.pdf', original_pdf)
+
+            # read the lines for each page stored along the PDF file
+            page_annotations = [
+                read_lines_document(self.fs, pdf.uid, page_uid)
+                for page_uid in pdf.pages
+            ]
+
+            # create a separate PDF for the annotations and merge it with the
+            # original PDF
+            logger.debug("Creating annotations PDF...")
+            create_annotations_pdf(page_annotations, annotations_pdf)
+            logger.debug("...done.")
+
+            logger.debug("Merging PDFs...")
+            merge_pdfs(original_pdf, annotations_pdf, merged_pdf)
+            logger.debug("...done.")
+
+            # read and return content of merged PDF
+            with open(merged_pdf, mode='rb') as f:
+                return f.read()
 
     def put_pdf(self, local, remote):
         '''Copy PDF ``local`` to folder ``remote``.'''
@@ -61,7 +114,7 @@ class RemarkableClient:
 
         meta = PDF_BASE_METADATA.copy()
         meta['visibleName'] = os.path.splitext(os.path.basename(local))[0]
-        meta['lastModified'] = str(arrow.utcnow().timestamp * 1000)
+        meta['lastModified'] = str(arrow.utcnow().timestamp() * 1000)
         meta['parent'] = parent_folder.uid
 
         content = PDF_BASE_CONTENT.copy()
