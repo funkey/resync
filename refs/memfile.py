dc6b924ad78976965c0665447d883b2888ea61f5
Jan Funke <funkej@janelia.hhmi.org>
2025-04-29 18:04:38 -0400

    Add attrs to MemFile

--- refs/memfile.py
+++ refs/memfile.py
@@ -1,4 +1,7 @@
+import llfuse
 import logging
+import stat
+import os
 
 logger = logging.getLogger(__name__)
 
@@ -10,18 +13,34 @@ class MemFile:
     data is held in memory.
     """
 
-    def __init__(self, data=None):
+    def __init__(self, attrs, data=None):
         self.data = data if data is not None else bytearray(b"")
+        self._attrs = attrs
         self.modified = False
 
     @property
     def size(self):
         return len(self.data)
 
+    @property
+    def inode(self):
+        return self._attrs.st_ino
+
+    @property
+    def attrs(self):
+        attrs = self._attrs
+        attrs.st_size = self.size
+        return attrs
+
     def read(self, length=None, offset=0):
         if length is None:
             length = self.size - offset
-        logger.debug("[MemFile::read] %d bytes @ %d offset", length, offset)
+        logger.debug(
+            "[MemFile::read] %d bytes @ %d offset of %d I have",
+            length,
+            offset,
+            self.size,
+        )
 
         if offset < self.size:
             if offset + length > self.size:
@@ -30,6 +49,7 @@ class MemFile:
         else:
             data = bytearray(b"")
 
+        logger.debug("[MemFile::read] return %d bytes", len(data))
         return bytes(data)
 
     def write(self, data, offset=0):
@@ -52,3 +72,17 @@ class MemFile:
         if length < self.size:
             self.data = self.data[:length]
             self.modified = True
+
+    def update_attrs(self, fields, attrs):
+        if fields.update_atime:
+            self._attrs.st_atime_ns = attrs.st_atime_ns
+        if fields.update_mtime:
+            self._attrs.st_mtime_ns = attrs.st_mtime_ns
+        if fields.update_mode:
+            self._attrs.st_mode = attrs.st_mode
+        if fields.update_uid:
+            self._attrs.st_uid = attrs.st_uid
+        if fields.update_gid:
+            self._attrs.st_gid = attrs.st_gid
+        if fields.update_size:
+            self._attrs.st_size = attrs.st_size
